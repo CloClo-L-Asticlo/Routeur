a35a03d324735d5cd5c6358fb1223cd657a62712
BACHIRI Chakib <bachiri@et.intechinfo.fr>
2021-06-01 15:32:06 +0200

    ajout formulaire dans acceuil + correction bdd

--- Modele/bdd_Connect.php
+++ Modele/bdd_Connect.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $bdd = new PDO('mysql:host=localhost; dbname=auto-route; charset=utf8', 'root', '');
+        $bdd = new PDO('mysql:host=localhost; dbname=Auto-Route; charset=utf8', 'root', '');
     } catch (Exception $E) {
         die ("error : " . $E->getMessage());
     }
